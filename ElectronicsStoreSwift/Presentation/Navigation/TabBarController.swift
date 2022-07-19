6c46e0b994d47c66cc1a63664e99bc4f64475679
wspr <wspr@gft.com>
2022-07-19 13:39:59 -0300

    Setting up the project

---
+++ ElectronicsStoreSwift/Presentation/Navigation/TabBarController.swift
@@ -0,0 +1,32 @@
+//
+//  TabBarController.swift
+//  ElectronicsStoreSwift
+//
+//  Created by Pinto Junior, William James on 19/07/22.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let homeVC = HomeViewController()
+        let cartVC = CartViewController()
+        let savedVC = SavedViewController()
+        let profileVC = ProfileViewController()
+        
+        self.setViewControllers([homeVC, cartVC, savedVC, profileVC], animated: false)
+        self.tabBar.tintColor = UIColor(named: "Primary")
+        self.tabBar.barTintColor = UIColor(named: "Disabled")
+
+        guard let items = self.tabBar.items else { return }
+        let icons = ["house.fill", "cart.fill", "bookmark.fill", "person.fill"]
+        
+        for i in 0...3 {
+            items[i].image = UIImage(systemName: icons[i])
+        }
+        
+    }
+}
---
+++ ElectronicsStoreSwift/Presentation/Saved/View/SavedViewController.swift
@@ -0,0 +1,60 @@
+//
+//  SavedViewController.swift
+//  ElectronicsStoreSwift
+//
+//  Created by Pinto Junior, William James on 19/07/22.
+//
+
+import UIKit
+
+class SavedViewController: UIViewController {
+    // MARK: - Constrants
+    // MARK: - Variables
+    // MARK: - Components
+    fileprivate let stackBase: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 32
+        stack.distribution = .fill
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    fileprivate let labelTitle: UILabel = {
+        let label = UILabel()
+        label.text = "Saved"
+        label.font = .systemFont(ofSize: 14, weight: .bold)
+        label.textColor = UIColor(named: "Text")
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupVC()
+    }
+    
+    // MARK: - Setup
+    fileprivate func setupVC() {
+        view.backgroundColor = UIColor(named: "Backgroud")
+        buildHierarchy()
+        buildConstraints()
+    }
+
+    // MARK: - Methods
+    fileprivate func buildHierarchy() {
+        view.addSubview(stackBase)
+        stackBase.addArrangedSubview(labelTitle)
+    }
+    
+    fileprivate func buildConstraints() {
+        NSLayoutConstraint.activate([
+            stackBase.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackBase.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackBase.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            stackBase.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+
+}
